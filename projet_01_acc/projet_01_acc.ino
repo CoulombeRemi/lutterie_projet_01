79bb2f14f4466e8b7204480c31be0d10db4d9cec
Unknown <rem.coulombe@gmail.com>
2020-03-22 18:37:47 -0400

    final

--- projet_01_acc/projet_01_acc.ino
+++ projet_01_acc/projet_01_acc.ino
@@ -13,11 +13,9 @@ const int OFSX = 0x1E; // valeur ofset pour calibration
 const int OFSY = 0x1F;
 const int OFSZ = 0x20;
 
-float x_f, y_f, z_f; // variable output du sensor
-int x_i_msb, y_i_msb, z_i_msb, x_i_lsb, y_i_lsb, z_i_lsb;
-char x1, x2, y1, y2, z1, z2;
+char x, y, z;
 float roll, pitch;
-int xx, yy, zz, servoX;
+int servoX;
 
 // Serial in for motor
 const int outPin = 6;
@@ -29,7 +27,7 @@ int posMoteur = 0; // center
 
 void setup() {
   // Serial
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(6, OUTPUT);
   // I2C
   Wire.begin();
@@ -39,35 +37,30 @@ void setup() {
   Wire.write(8); // le sensor s'allume
   Wire.endTransmission();
   delay(10);
-  
-  calibration();
+
+ 
+  // calibration
+    // x 
+  Wire.beginTransmission(SENSOR);
+  Wire.write(OFSX);
+  Wire.write(20);
+  Wire.endTransmission();
+  delay(10);
+    // y
+  Wire.beginTransmission(SENSOR);
+  Wire.write(OFSY);
+  Wire.write(81);
+  Wire.endTransmission();
+  delay(10);
+    // z
+  Wire.beginTransmission(SENSOR);
+  Wire.write(OFSZ);
+  Wire.write(8);
+  Wire.endTransmission();
+  delay(37);
 
   // servo init pin
   moteur.attach(9);
-}
-void calibration(){
-  /*
-  value - 256 = i
-  correction = -round(i/4)
-  */  
-  // x
-  Wire.beginTransmission(SENSOR);
-  Wire.write(OFSX);
-  Wire.write(-5);
-  Wire.endTransmission();
-  delay(10);
-  // y
-  Wire.beginTransmission(SENSOR);
-  Wire.write(OFSY);
-  Wire.write(69);
-  Wire.endTransmission();
-  delay(10);
-  // z
-  Wire.beginTransmission(SENSOR);
-  Wire.write(OFSZ);
-  Wire.write(64);
-  Wire.endTransmission();
-  delay(10);
 }
 
 void loop() {
@@ -78,68 +71,53 @@ void loop() {
   Wire.requestFrom(SENSOR, 6, true); 
   // i2c : MSB first
   // output en two's complement
-  x1 = Wire.read();
-  x2 = Wire.read();
-  y1 = Wire.read();
-  y2 = Wire.read();
-  z1 = Wire.read();
-  z2 = Wire.read();
-  
+
+  x = (Wire.read() | Wire.read()<<8);
+  y = (Wire.read() | Wire.read()<<8);
+  z = (Wire.read() | Wire.read()<<8);
+
+  roll = atan(y / sqrt(pow(x, 2) + pow(z, 2))) * 180 / PI;
+  pitch = atan(-1 * x / sqrt(pow(y, 2) + pow(z, 2))) * 180 / PI;
   //******** Test print**********
   //testPrint();
   //******** Send Serial **********
   sendToSerial();
- 
-  // control moteur
-  int packetSize = 0;
-  packetSize = SLIPSerialRead(slipPacket);
-  packetSize = SLIPSerialRead( slipPacket );
-  for (int i=0 ; i < packetSize; i++) {
-    analogWrite(outPin, slipPacket[i]);
-  }
-  // control servo
-  servoX = x1|x2<<8;
-  //posMoteur = 0;
-  posMoteur = map(servoX, -255, 255, 0, 180);
+
+  int posi = (x-100)*5;
+  posMoteur = map(posi, -255, 255, 0, 180);
   moteur.write(posMoteur);
 }
 
 /*----------------------------------*/
 void testPrint(){
-  xx = (x1 | x2 << 8);
-  yy = (y1 | y2 << 8);
-  zz = (z1 | z2 << 8);
-/*
-  xx = xx/64;
-  yy = yy/64;
-  zz = zz/64;
-  */
+
+  float ax = x/256;
+  float ay = y/256;
+  float az = z/256;
+  
   Serial.print("x ");
-  Serial.print(xx);
+  Serial.print(ax);
   Serial.print(" y ");
-  Serial.print(yy);
+  Serial.print(ay);
   Serial.print(" z ");
-  Serial.print(zz);
+  Serial.print(az);
   Serial.print("\n");
-  roll = atan(yy / sqrt(pow(xx, 2) + pow(zz, 2))) * 180 / PI;
-  pitch = atan(-1 * xx / sqrt(pow(yy, 2) + pow(zz, 2))) * 180 / PI;
- /* Serial.print("roll ");
-  Serial.print(roll);
-  Serial.print(" pitch ");
-  Serial.print(pitch);
-  Serial.print("\n");*/
 }
 
 /*----------------------------------*/
 void sendToSerial(){
-  SLIPSerialWrite(x1);
-  SLIPSerialWrite(x2);
-  SLIPSerialWrite(y1);
-  SLIPSerialWrite(y2);
-  SLIPSerialWrite(z1);
-  SLIPSerialWrite(z2);
+  // x
+  SLIPSerialWrite(x>>8);
+  SLIPSerialWrite(x&255);
+  // y
+  SLIPSerialWrite(y>>8);
+  SLIPSerialWrite(y&255);
+  // z
+  SLIPSerialWrite(z>>8);
+  SLIPSerialWrite(z&255);
+
   Serial.write(END);
-  delay(2);
+  delay(3);
 }
 void SLIPSerialWrite(int value){
   if(value == END){ 
@@ -154,37 +132,4 @@ void SLIPSerialWrite(int value){
     Serial.write(value);
     return;
   }
-}
-
-// pour moteur
-
-int SLIPSerialRead(byte * slipPacket){
-  int packetIndex = 0;
-  boolean escape = false;
-  boolean packetComplete = false;
-  if(Serial.available() == 0){
-    return 0;
-  }
-  while(!packetComplete){
-    if(Serial.available()>0){
-      byte b = Serial.read();
-      if(escape){
-        if(b == ESC_END){
-          slipPacket[packetIndex] = END;
-        }else if(b == ESC_ESC){
-          slipPacket[packetIndex] = ESC;
-        }
-        packetIndex++;
-        escape = 0;
-      }else if(b == END){
-        packetComplete = true;
-      }else if(b == ESC){
-        escape = 1;
-      }else{
-        slipPacket[packetIndex] = b;
-        packetIndex++;
-      }
-    }
-  }
-  return packetIndex;
 }
